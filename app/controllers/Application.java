ccf6c7d1c5696bfbadd4ed0018e713b55908582a
lrk66 <lrk66@cornell.edu>
2015-09-05 16:17:37 -0400

    initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.*;
+
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+   
+  public Result index() {
+    return ok(index.render("Your new application is ready."));
+  }
+  
+  public Result tasks() {
+    return TODO;
+  }
+  
+  public Result newTask() {
+    return TODO;
+  }
+  
+  public Result deleteTask(Long id) {
+    return TODO;
+  }
+}
+  
